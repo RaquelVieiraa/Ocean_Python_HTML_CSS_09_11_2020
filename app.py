8617d3193baa5a56c446b4c68f67a24a6b0463d0
RaquelVieiraa <raquellvieiraa@gmail.com>
2020-11-10 15:54:23 -0300

    Estilização da pagina + primeira parte do codigo python

--- app.py
+++ app.py
@@ -9,8 +9,24 @@ app = Flask(__name__)
 bp = Blueprint('app', __name__)
 
 @bp.route('/', methods=('GET', 'POST'))
+
 def index():
-    return render_template('index.html')
+    nome_jogador = None
+    premio = None
+    premio_imagem = None
+
+    if request.method == 'POST':
+        nome_jogador = request.form['nome_jogador']
+        escolha = request.form['escolha']
+
+        if escolha == 'futurista':
+            premio = 'um sabre de luz'
+            premio_imagem = ''
+        elif escolha == 'medieval':
+           premio = 'uma espada'
+           premio_imagem = ''
+
+    return render_template('index.html', nome_jogador=nome_jogador, premio=premio, premio_imagem=premio_imagem)
 
 
 app.register_blueprint(bp)
